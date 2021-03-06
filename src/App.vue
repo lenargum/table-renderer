530b0dd6b3a715ad94e6d1259f72f689f0987e64
lenargum <lenargamgam@gmail.com>
2021-03-06 19:13:23 +0300

    table header draft

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,11 @@ export default {
 </script>
 
 <style>
+
+* {
+  font-family: "Roboto", sans-serif;
+}
+
 /*
 Сброс стилей CSS
 https://medium.com/@stasonmars/%D1%81%D0%BE%D0%B2%D1%80%D0%B5%D0%BC%D0%B5%D0%BD%D0%BD%D1%8B%D0%B8%CC%86-%D1%81%D0%B1%D1%80%D0%BE%D1%81-css-f5816963c82b
--- src/components/TableRenderer.vue
+++ src/components/TableRenderer.vue
@@ -1,15 +1,51 @@
 <template>
-  <div>
-
-  </div>
+  <table class="table table-striped">
+    <thead>
+    <tr v-for="(row,i) in headColumnsData" :key="i">
+      <td v-for="cell in row" :colspan="cell.width" :rowspan="cell.height" :key="cell.id">{{ cell.name }}</td>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+      <td>Lorem ipsum dolor sit amet.</td>
+      <td>Culpa id ipsa quod veritatis?</td>
+      <td>Doloremque magni perferendis qui sequi.</td>
+      <td>Culpa laborum nesciunt quisquam sint.</td>
+      <td>Animi non qui quibusdam voluptatibus!</td>
+      <td>Ipsa maxime quas quod recusandae.</td>
+      <td>Dolores fuga iusto qui saepe?</td>
+      <td>Assumenda deleniti ipsam possimus voluptate.</td>
+      <td>Aperiam cum dolorem laudantium praesentium?</td>
+    </tr>
+    </tbody>
+  </table>
 </template>
 
 <script>
+import columnData from "@/data/data.json"
+
 export default {
-name: "TableRenderer"
+  name: "TableRenderer",
+  data() {
+    return {
+      headColumnsData: columnData.head
+    }
+  }
 }
 </script>
 
 <style lang="scss">
+table {
+  text-align: center;
+  border-collapse: collapse;
+  margin: 10px;
+}
 
+thead {
+  font-weight: bold;
+}
+
+td {
+  border: 1px solid lightblue;
+}
 </style>
